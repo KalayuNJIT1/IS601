55541af5898febc743e4fb3f152f61d8e8470588
KalayuNJIT1 <ka669@njit.edu>
2025-02-26 19:13:14 -0500

    Made some changes.

--- test.py
+++
@@ -1,6 +0,0 @@
-import json
-
-class midterm_project():
- def __init__(self,filename):
-  with open(filename) as file:
-   self.data=json.load(file)
--- test_orders.py
+++
@@ -1,6 +0,0 @@
-import json
-
-def process_orders(input_file):
-    # Step 1: Read the orders from the input file
-    with open(input_file, 'r') as file:
-        orders = json.load(file)
